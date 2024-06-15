86625eeab599e3874a56a8b2c9bad35b2f965363
Dilmurat <grxndxx@gmail.com>
2024-06-15 19:29:15 +0500

    upd 444

--- main.go
+++ main.go
@@ -5,7 +5,7 @@ import "net/http"
 func main() {
 	server := http.NewServeMux()
 	server.HandleFunc("/hello", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("Hello, Dilmurat!"))
+		w.Write([]byte("Hello, Dilmurat!!"))
 	})
 	http.ListenAndServe(":8080", server)
 }
